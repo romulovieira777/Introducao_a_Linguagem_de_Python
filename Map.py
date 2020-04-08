25c205c0bbe0de018d02e7a225a09488bc4cb0f5
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-08 11:22:27 -0300

    Map

---
+++ Map.py
@@ -0,0 +1,31 @@
+# Map
+'''
+def dobro (x):
+    return x*2
+
+valor = 2
+
+print(dobro(valor))
+'''
+'''
+def dobro (x):
+    return x*2
+
+valor = [1, 2, 3, 4, 5]
+
+valor_dobrado = map(dobro, valor)
+
+for v in valor_dobrado:
+    print(v)
+'''
+
+def dobro (x):
+    return x*2
+
+valor = [1, 2, 3, 4, 5]
+
+valor_dobrado = map(dobro, valor)
+
+valor_dobrado = list(valor_dobrado)
+
+print(valor_dobrado)
