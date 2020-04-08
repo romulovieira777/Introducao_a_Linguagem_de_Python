e908d13ff986a252795a5545ef1d19a1d65b57b2
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-08 11:20:53 -0300

    Enumerate

---
+++ enumerate.py
@@ -0,0 +1,11 @@
+# Função enumerate
+'''
+lista = ['abacate', 'bola', 'cachorro']
+
+for i in range(len(lista)):
+    print(i, lista[i])
+'''
+lista = ['abacate', 'bola', 'cachorro']
+
+for i, nome in enumerate(lista):
+    print(i, nome)
