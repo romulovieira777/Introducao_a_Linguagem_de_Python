d35d89c94e85382128be2a3872b60578924ca935
romulovieira777 <61069548+romulovieira777@users.noreply.github.com>
2020-04-08 11:21:30 -0300

    Filter

---
+++ Filter.py
@@ -0,0 +1,22 @@
+# Filter
+'''
+def pares(i):
+    if i % 2 == 0:
+        return i
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+lista_pares = filter(pares, lista)
+
+print(list(lista_pares))
+'''
+
+def impar(i):
+    if i % 2 == 1:
+        return i
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+lista_pares = filter(pares, lista)
+
+print(list(lista_pares))
